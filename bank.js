84e78a8035cb804e7a95801be9859308e4a90028
mehideopu <mehideopu11@gmail.com>
2021-01-28 11:33:20 +0600

    first commit

---
+++ bank.js
@@ -0,0 +1,62 @@
+
+       
+     //deposit button event hand//
+    //  const depositBtn = document.getElementById("addDeposit");
+    //  depositBtn.addEventListener("click", function () {
+    //      const depositeAmount = document.getElementById("dpositdeeAmount").value;
+    //      const depositNumber = parseFloat(depositeAmount);
+
+    //      // const currentDeposit = document.getElementById("currentDeposit").innerText;
+    //      // const currentDepositNumb = parseFloat(currentDeposit);
+    //      // const totalDeposit =  depositNumber+ currentDepositNumb;
+    //      // document.getElementById("currentDeposit").innerText = totalDeposit;
+    //      updateSpamTex("currentDeposit", depositNumber);
+    //      updateSpamTex("currentBalance", depositNumber);
+
+    //      document.getElementById("currentDeposit").value = "";
+    //  })
+
+    //  function updateSpamTex(id, depositNumber) {
+    //      const currentBalance = document.getElementById(id).innerText;
+    //      const currentBalanceNumber = parseFloat(currentBalance);
+
+    //      const totalBalance = depositNumber + currentBalanceNumber;
+    //      document.getElementById(id).innerText = totalBalance;
+    //  }
+
+    //  //withdraw amount evan
+    //  const withdrawBtn = document.getElementById("addwithdraw");
+    //  withdrawBtn.addEventListener("click", function () {
+    //      const withdrawAmount = document.getElementById("withdrawAmount").value;
+    //      const withdrawNumber = parseFloat(withdrawAmount);
+    //  })
+
+
+
+
+
+        
+    //     const loginBtn = document.getElementById("login");
+    //     loginBtn.addEventListener("click", function () {
+    //         const loginArea = document.getElementById("log-area");
+    //         loginArea.style.display = "none";
+
+    //         const transactionArea = document.getElementById("transaction-area");
+    //         transactionArea.style.display = "block";
+    //     })
+    //     //deposit button event hand//
+    //     const depositBtn = document .getElementById ("addDeposit");
+    //     depositBtn. addEventListener("click" , function(){
+    //         //console.log ("deposit clicked");
+    //     const depositeAmount = document.getElementById("depositeAmount").Value;
+    //     const depositNumber = parseFloat (depositeAmount);
+
+    //     const  currentDeposit = document.getElementById("currentDeposit").innerText;
+    //     const currentDepositNumb = parseFloat (currentDeposit);
+    //     const totalDeposit =  depositNumber+ currentDepositNumb;
+    //     //console.log ('totalDeposit');
+    //     document.getElementById("currentDeposit").innerText = totalDeposit;
+    //     document. getElementById ("currentDeposit").Value= "";
+    //     })  
+        
+    //     //balance
